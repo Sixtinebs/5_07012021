ea2d7ff32c4f9bf4d8b3b8bcaf1e9c04ba96274e
Sixtine BS <sixtine.bs11@gmail.com>
2021-01-08 09:23:29 +0100

    display product

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
 
 const $newDiv = document.createElement('div');
 let $produits = document.getElementById('produits');
-console.log($produits);
-
-
 
 const $request = new XMLHttpRequest();
 $request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         const $response = JSON.parse(this.responseText);
         $produits.innerHTML = '<p>' + $response[0].name + '</p>';
-        console.log($response);
-        function resName() {
+        $cardProduit = [];
+        function respName() {
             for(let i in $response) {
-                console.log($response[i].name);
-                $produits.innerHTML = ' <div class="card mb-3" style="max-width: 540px;"><div class="row g-0"><div class="col-md-4"><img src="..." alt="..."></div><div class="col-md-8"><div class="card-body"><h5 class="card-title">'+ $response[i].name +'</h5><p class="card-text">'+ $response[i].description +'</p><p class="card-text"><small class="text-muted">' + $response[i].price +  '</small></p></div></div></div></div>';
-
+                $cardProduit.push(' <div class="card mb-3" style="max-width: 540px;"><div class="row g-0"><div class="col-md-4"><img src="'+ $response[i].imageUrl +'" alt="..." style="width: 100%;" ></div><div class="col-md-8"><div class="card-body"><h5 class="card-title">'+ $response[i].name +'</h5><p class="card-text">'+ $response[i].description +'</p><p class="card-text"><small class="text-muted">' + $response[i].price +  '</small></p></div></div></div></div>');
+                $produits.innerHTML = $cardProduit;
             }
+
+            
         }
-        resName();
-       
+        console.log($cardProduit); 
+        respName();
     }
 };
 
